41e65f0ec5405bdcef964e6c4db7e4c6fe371c5e
ye1223 <1374003554@qq.com>
2024-03-23 16:16:46 +0800

    ✨ feat: Table UI Component V1

--- src/components/UI/Table.vue
+++ src/components/UI/Table.vue
@@ -1,20 +1,73 @@
 <script lang="ts" setup>
-import { GoodsInfo } from '@/ts/interfaces/goods.interface'
+import { ref } from 'vue'
 
 /* interface TableRow<T extends Record<string, any>> {
 	data: T
 } */
-const props = defineProps<{
-	labels: string[]
-	tableData: {
-		total: number
-		rows: GoodsInfo[]
-	}
-	operation?: boolean
-	selectAll?: boolean
+// const props = defineProps<{
+// 	no?: boolean
+// 	labels: string[]
+// 	list: Array<any>
+// 	operation?: boolean
+// 	selectAll?: boolean
+// }>()
+interface Props {
+	no?: boolean // system serial number
+	labels: string[] // table head
+	list: Array<any> // table row data
+	operation?: boolean // whether have operation
+	selectAll?: boolean // 
+	checkbox?: boolean // 多选框
+}
+const props = withDefaults(defineProps<Props>(), {
+	no: true,
+	operation: false,
+	selectAll: false
+})
+
+const emit = defineEmits<{
+	offerSelectedData: [row: any] // 具名元组语法
 }>()
 
-const handleAllChanged = () => {}
+const selectedRows = ref(new Set<any>())
+const singleSelectMode = ref(true) // 单选模式
+
+const handleSelectionChange = (row: any) => {
+	if (singleSelectMode.value) {
+		selectedRows.value.clear()
+		selectedRows.value.add(row)
+		emit('offerSelectedData', { ...[...selectedRows.value][0] })
+		console.log({ ...[...selectedRows.value][0] })
+	} else {
+		const rowIndex = props.list.findIndex(item => item === row)
+		if (rowIndex !== -1) {
+			if (selectedRows.value.has(rowIndex)) {
+				selectedRows.value.delete(rowIndex)
+			} else {
+				selectedRows.value.add(rowIndex)
+			}
+		}
+	}
+}
+
+const handleAllChanged = (event: Event) => {
+	const target = event.target as HTMLInputElement
+	if (target.checked) {
+		if (singleSelectMode.value) {
+			selectedRows.value.clear()
+		}
+		selectedRows.value.clear()
+		props.list.forEach((_, index) => selectedRows.value.add(index))
+	} else {
+		selectedRows.value.clear()
+	}
+}
+
+/* const toggleSingleSelectMode = () => {
+	singleSelectMode.value = !singleSelectMode.value
+} */
+
+const isSelected = (rowIndex: number) => selectedRows.value.has(rowIndex)
 </script>
 
 <template>
@@ -26,7 +79,7 @@ const handleAllChanged = () => {}
 				class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
 			>
 				<tr>
-					<th class="px-6 py-3" v-if="props.selectAll">
+					<th class="px-6 py-3" v-if="props.checkbox">
 						<!-- 全选checkbox -->
 						<div class="flex items-center">
 							<input
@@ -44,7 +97,9 @@ const handleAllChanged = () => {}
 							</label>
 						</div>
 					</th>
-					<th scope="col" class="px-6 py-3">系统编号</th>
+					<th scope="col" v-if="props.no" class="px-6 py-3">
+						系统编号
+					</th>
 					<th
 						scope="col"
 						class="px-6 py-3"
@@ -59,7 +114,7 @@ const handleAllChanged = () => {}
 			<tbody>
 				<tr
 					class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700"
-					v-for="(row, index) in props.tableData.rows"
+					v-for="(row, index) in props.list"
 					:key="index"
 				>
 					<!-- <th
@@ -68,12 +123,14 @@ const handleAllChanged = () => {}
 					>
 						{{ row.id }}
 					</th> -->
-					<th class="text-center" v-if="props.selectAll">
+					<th class="text-center" v-if="props.checkbox">
 						<input
 							id="checkbox"
 							type="checkbox"
 							value=""
 							class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+							:checked="isSelected(index)"
+							@change="handleSelectionChange(row)"
 						/>
 					</th>
 					<th
